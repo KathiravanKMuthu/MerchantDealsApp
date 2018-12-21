61aadfaa96490111c307bb11fac511314de5bdf8
KathiravanKMuthu <kathiravan.muthu@gmail.com>
2018-12-21 22:25:22 +0530

    Merchant App updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Storage } from '@ionic/storage';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = "LoginPage";
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, storage: Storage) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,43 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { AuthService } from '../providers/auth-service';
+import { HttpModule } from '@angular/http';
+import { SpinnerProvider } from '../providers/spinner';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { DealsService } from '../providers/deals-service';
+import { CallNumber } from '@ionic-native/call-number';
+import { LaunchNavigator } from '@ionic-native/launch-navigator';
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    BrowserAnimationsModule,
+    HttpModule, NgxQRCodeModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot({
+      name: "__mydb",
+      driverOrder: ['sqlite', 'websql', 'indexeddb']
+    })
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    StatusBar, SplashScreen, DealsService, LaunchNavigator,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthService, SpinnerProvider, BarcodeScanner, CallNumber
   ]
 })
 export class AppModule {}
